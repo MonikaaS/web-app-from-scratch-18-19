a5cedc4a3550a4d44bc270d4abc9a55dd253d664
Monika <monika_streljajeva@hotmail.com>
2019-02-04 15:09:52 +0100

    Render the data in html

--- week1/public/js/app.js
+++ week1/public/js/app.js
@@ -1,6 +1,13 @@
 'use strict'
 
-//https://www.taniarascia.com/how-to-connect-to-an-api-with-javascript/ used this as exmple
+//https://www.taniarascia.com/how-to-connect-to-an-api-with-javascript/ used this as an example
+const app = document.getElementById('root')
+
+const container = document.createElement('div')
+container.setAttribute('class', 'container')
+
+app.appendChild(container)
+
 var request = new XMLHttpRequest()
 
 request.open('GET', 'https://api.thecatapi.com/v1/breeds', true)
@@ -8,9 +15,28 @@ request.open('GET', 'https://api.thecatapi.com/v1/breeds', true)
 request.onload = function () {
     var data = JSON.parse(this.response)
 
-    data.forEach(cat => {
-        console.log(cat.name)
-    });
+    if (request.status >= 200 && request.status < 400) {
+        data.forEach(cat => {
+
+            const card = document.createElement('div')
+            card.setAttribute('class', 'card')
+
+            const h1 = document.createElement('h1')
+            h1.innerHTML = cat.name
+
+            const p = document.createElement('p')
+            p.innerHTML = cat.description
+
+            container.appendChild(card)
+
+            card.appendChild(h1)
+            card.appendChild(p)
+            console.log(cat)
+            //cat.name, cat.discription, cat.origin, cat.temperament, cat.weight.metric
+        })
+    } else {
+        console.log('error')
+    }
 }
 
 // Send the request

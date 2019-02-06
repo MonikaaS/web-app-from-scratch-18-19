e2fafd632f9ab7c47fca3922b997319de1155a31
Monika <monika_streljajeva@hotmail.com>
2019-02-06 13:58:20 +0100

    Changed cat api to GoT api

--- week1/public/js/app.js
+++ week1/public/js/app.js
@@ -10,28 +10,28 @@ app.appendChild(container)
 
 var request = new XMLHttpRequest()
 
-request.open('GET', 'https://api.thecatapi.com/v1/breeds', true)
+request.open('GET', 'https://api.got.show/api/houses/', true)
 
 request.onload = function () {
     var data = JSON.parse(this.response)
 
     if (request.status >= 200 && request.status < 400) {
-        data.forEach(cat => {
+        data.forEach(characters => {
 
             const card = document.createElement('div')
             card.setAttribute('class', 'card')
 
             const h1 = document.createElement('h1')
-            h1.innerHTML = cat.name
+            h1.innerHTML = characters.name
 
             const p = document.createElement('p')
-            p.innerHTML = cat.description
+            p.innerHTML = characters.region
 
             container.appendChild(card)
 
             card.appendChild(h1)
             card.appendChild(p)
-            console.log(cat)
+            console.log(characters)
             //cat.name, cat.discription, cat.origin, cat.temperament, cat.weight.metric
         })
     } else {

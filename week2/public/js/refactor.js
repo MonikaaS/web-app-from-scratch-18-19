5115c7f1ddb5700f30ca3ea5312dec63025e2ea3
Monika <monika_streljajeva@hotmail.com>
2019-02-13 15:06:17 +0100

    Attempt at routie, loads only the index

--- week2/public/js/refactor.js
+++ week2/public/js/refactor.js
@@ -1,55 +1,88 @@
-//router
-var router = {
-    overview: function () {
-        api.get().then(data => {
-            render.overview(data)
-        })
-    }
-}
-//api
-var api = {
-    get: function () {
-        return new Promise(function (resolve, reject) {
-            var url = 'https://api.got.show/api/houses/';
+    (function () {
+        'use strict'
 
-            var request = new XMLHttpRequest();
-            request.open('GET', url, true)
+        //router
+        var router = {
+            overview: function () {
+                api.get().then(data => {
+                    routie("/", () => {
+                        render.overview(data)
+                        console.log('dit is de home')
+                    })
+                });
+                routie("/")
+            },
+            detail: function () {
+                api.get().then(data => {
+                    console.log(data._id)
+                    routie(":56fa9ae4c9e167a3645523cc", () => {
+                        render.detail(data)
+                        console.log('dit is de detail')
+                    })
+                })
+            }
+        }
 
-            request.onload = () => {
-                if (request.status >= 200 && request.status < 400) {
-                    // Success!
-                    var data = JSON.parse(request.responseText);
-                    resolve(data)
-                } else {
-                    // We reached our target server, but it returned an error
-                    reject(error)
-                }
-            };
+        //api
+        var api = {
+            get: function () {
+                return new Promise(function (resolve, reject) {
+                    var url = 'https://api.got.show/api/houses/';
 
-            request.onerror = () => {
-                // There was a connection error of some sort
-                console.log('error')
-            };
+                    var request = new XMLHttpRequest();
+                    request.open('GET', url, true)
 
-            request.send()
-        });
-    }
-};
+                    request.onload = () => {
+                        if (request.status >= 200 && request.status < 400) {
+                            // Success!
+                            var data = JSON.parse(request.responseText);
+                            resolve(data)
+                            console.log(data)
+                        } else {
+                            // We reached our target server, but it returned an error
+                            reject(error)
+                        }
+                    };
 
-//render
-var render = {
-    overview: data => {
-        var app = document.getElementById('container')
+                    request.onerror = () => {
+                        // There was a connection error of some sort
+                        console.log('error')
+                    };
 
-        data.forEach((house, i) => {
-            var html = `
+                    request.send()
+                });
+            }
+        };
+
+        //render
+        var render = {
+            overview: data => {
+                var app = document.getElementById('container')
+
+                data.forEach((house, i) => {
+                    var html = `
                 <div class="card">
                 <a href='/#${house._id}'>${house.name}</a>
                 <p>region: ${house.region == undefined ? "no region" : house.region}</p>
                 </div>`;
-            app.insertAdjacentHTML('beforeend', html);
+                    app.insertAdjacentHTML('beforeend', html);
+                })
+            },
+            detail: data => {
+                var app = document.getElementById('container')
 
-        })
-    }
-}
-router.overview()
+                data.forEach((detail, i) => {
+                    var html = `
+                <div class="card">
+                <a href='/#${detail._id}'>${detail.name}</a>
+                <p>region: ${detail.region == undefined ? "no region" : detail.region}</p>
+                <p>region: ${detail.coatOfArms == undefined ? "no coat of arms" : detail.coatOfArms}</p>
+                </div>`;
+                    app.insertAdjacentHTML('beforeend', html);
+                })
+            }
+        }
+
+        // start the application
+        router.overview()
+    })()

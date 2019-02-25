8298313e3a41b8cc5ffe507fb90c0c649e7614a0
Monika <monika_streljajeva@hotmail.com>
2019-02-25 15:17:59 +0100

    centered links and heading

--- week2/public/js/refactor.js
+++ week2/public/js/refactor.js
@@ -1,63 +1,44 @@
 (function () {
-    "use strict";
+    "use strict"
 
     //app
     const app = {
         init: function () {
-            console.log('App: Init');
+            console.log('App: Init')
             // TODO: Setup Hashchange/HistoryAPI URL listening
             // Reload the current page, or the homepage if no id and data is present
         }
     }
 
-    // //router
-    // var router = {
-    //     overview: function () {
-    //         api.get().then(data => {
-    //             routie("/", () => {
-    //                 render.overview(data);
-    //                 console.log("dit is de home");
-    //             });
-    //         });
-    //         routie("/");
-    //     },
-    //     detail: function () {
-    //         routie("/:id", id => {
-    //             render.detail(id);
-    //             console.log("dit is de detail");
-    //         });
-    //     }
-    // };
-
     //api
     var api = {
         get: function () {
             return new Promise(function (resolve, reject) {
-                var url = "https://api.got.show/api/houses/";
+                var url = "https://api.got.show/api/houses/"
 
-                var request = new XMLHttpRequest();
-                request.open("GET", url, true);
+                var request = new XMLHttpRequest()
+                request.open("GET", url, true)
 
                 request.onload = id => {
                     if (request.status >= 200 && request.status < 400) {
                         // Success!
-                        var data = JSON.parse(request.responseText);
-                        resolve(data);
+                        var data = JSON.parse(request.responseText)
+                        resolve(data)
                     } else {
                         // We reached our target server, but it returned an error
                         reject(error);
                     }
-                };
+                }
 
                 request.onerror = () => {
                     // There was a connection error of some sort
-                    console.log("error");
-                };
+                    console.log("error")
+                }
 
                 request.send();
-            });
+            })
         }
-    };
+    }
 
     //render
     var render = {
@@ -72,50 +53,61 @@
                 <p>region: ${
                   house.region == undefined ? "no region" : house.region
                 }</p>
-                </div>`;
-                app.insertAdjacentHTML("beforeend", html);
-            });
+                </div>`
+                app.insertAdjacentHTML("beforeend", html)
+            })
         },
         detail: function (id) {
             var app = document.getElementById("container");
             app.innerHTML = ''
 
+            console.log(id)
+
             id.forEach(id => {
                 var html = `
                 <div class="card">
                 <h2>${id.name}</h2>
+                <p>title: ${
+                    id.title == undefined ? "no title" : id.title
+                }</p>
+                <p>current lord: ${
+                    id.currentLord == undefined ? "no current lord" : id.currentLord
+                }</p>
+                <p>overlord: ${
+                    id.overlord == undefined ? "no overlord" : id.overlord
+                }</p>
                 <p>region: ${
                     id.region == undefined ? "no region" : id.region
                 }</p>
                 <p>coat of arms: ${
                     id.coatOfArms == undefined ? "no coat of arms" : id.coatOfArms
                 }</p>
-                </div>`;
-                app.insertAdjacentHTML("beforeend", html);
-            });
+                </div>`
+                app.insertAdjacentHTML("beforeend", html)
+            })
         }
-    };
+    }
 
     //routie
     routie({
         '/': function () {
             api.get().then(data => {
-                render.overview(data);
-                console.log("dit is de home");
-            });
+                render.overview(data)
+                console.log("dit is de home")
+            })
         },
         '/:id': function (id) {
             api.get().then(data => {
-                var specificId = data.filter(x => {
-                    return x._id == id
+                var specificId = data.filter(item => {
+                    return item._id == id
                 })
 
                 render.detail(specificId);
-                console.log("dit is de detail");
-            });
+                console.log("dit is de detail")
+            })
         }
-    });
+    })
 
     // start the application
     app.init()
-})();
+})()

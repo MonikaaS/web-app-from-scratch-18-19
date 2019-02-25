5a41db79b5df4e52556036836d7c068399efa991
Monika <monika_streljajeva@hotmail.com>
2019-02-25 12:57:53 +0100

    Fixed routie

--- week2/public/js/refactor.js
+++ week2/public/js/refactor.js
@@ -1,84 +1,95 @@
-    (function () {
-        'use strict'
+(function () {
+    "use strict";
 
-        //router
-        var router = {
-            overview: function () {
-                api.get().then(data => {
-                    routie("/", () => {
-                        render.overview(data)
-                        console.log('dit is de home')
-                    })
+    //router
+    var router = {
+        overview: function () {
+            api.get().then(data => {
+                routie("/", () => {
+                    render.overview(data);
+                    console.log("dit is de home");
                 });
-                routie("/")
-            },
-            detail: function () {
-                routie(":id", id => {
-                    render.detail(id)
-                    console.log('dit is de detail')
-                })
-            }
+            });
+            routie("/");
+        },
+        detail: function () {
+            routie("/:id", id => {
+                render.detail(id);
+                console.log("dit is de detail");
+            });
         }
+    };
 
-        //api
-        var api = {
-            get: function () {
-                return new Promise(function (resolve, reject) {
-                    var url = 'https://api.got.show/api/houses/';
+    //api
+    var api = {
+        get: function () {
+            return new Promise(function (resolve, reject) {
+                var url = "https://api.got.show/api/houses/";
 
-                    var request = new XMLHttpRequest();
-                    request.open('GET', url, true)
+                var request = new XMLHttpRequest();
+                request.open("GET", url, true);
 
-                    request.onload = id => {
-                        if (request.status >= 200 && request.status < 400) {
-                            // Success!
-                            var data = JSON.parse(request.responseText);
-                            resolve(data)
-                            router.detail(id)
-                            console.log(data)
-                        } else {
-                            // We reached our target server, but it returned an error
-                            reject(error)
-                        }
-                    };
+                request.onload = id => {
+                    if (request.status >= 200 && request.status < 400) {
+                        // Success!
+                        var data = JSON.parse(request.responseText);
+                        resolve(data);
 
-                    request.onerror = () => {
-                        // There was a connection error of some sort
-                        console.log('error')
-                    };
+                        router.detail(id);
+                        console.log(data);
+                    } else {
+                        // We reached our target server, but it returned an error
+                        reject(error);
+                    }
+                };
 
-                    request.send()
-                });
-            }
-        };
+                request.onerror = () => {
+                    // There was a connection error of some sort
+                    console.log("error");
+                };
 
-        //render
-        var render = {
-            overview: data => {
-                var app = document.getElementById('container')
+                request.send();
+            });
+        }
+    };
 
-                data.forEach((house, i) => {
-                    var html = `
-                <div class="card">
-                <a href="${'http://127.0.0.1:5500/week2/index.html'+'#'+(house._id)}">${house.name}</a>
-                <p>region: ${house.region == undefined ? "no region" : house.region}</p>
-                </div>`;
-                    app.insertAdjacentHTML('beforeend', html);
-                })
-            },
-            detail: function (x) {
-                var app = document.getElementById('container')
-                console.log(x)
+    //render
+    var render = {
+        overview: data => {
+            var app = document.getElementById("container");
+            app.innerHTML = ''
+
+            data.forEach((house, i) => {
                 var html = `
                 <div class="card">
-                <a href="${'http://127.0.0.1:5500/week2/index.html'+'#'+(x._id)}">${x.name}</a>
-                <p>region: ${x.region == undefined ? "no region" : x.region}</p>
-                <p>region: ${x.coatOfArms == undefined ? "no coat of arms" : x.coatOfArms}</p>
+                <a href="${
+                  "#/" +
+                  house._id}">${house.name}</a>
+                <p>region: ${
+                  house.region == undefined ? "no region" : house.region
+                }</p>
                 </div>`;
-                app.insertAdjacentHTML('beforeend', html);
-            }
+                app.insertAdjacentHTML("beforeend", html);
+            });
+        },
+        detail: function (id) {
+            var app = document.getElementById("container");
+            app.innerHTML = ''
+            console.log(id);
+            var html = `
+                <div class="card">
+                <p>${id._id}</p>
+                <p>region: ${
+                  id.region == undefined ? "no region" : id.region
+                }</p>
+                <p>region: ${
+                  id.coatOfArms == undefined ? "no coat of arms" : id.coatOfArms
+                }</p>
+                </div>`;
+            app.insertAdjacentHTML("beforeend", html);
         }
+    };
 
-        // start the application
-        router.overview()
-    })()
+    // start the application
+    router.overview();
+})();

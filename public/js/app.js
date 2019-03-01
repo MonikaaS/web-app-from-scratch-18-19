1a58ade2f8954cc4d75460afc96d43d3b8b6e1aa
Monika <monika_streljajeva@hotmail.com>
2019-03-01 10:09:46 +0100

    removed error from catch

--- public/js/app.js
+++ public/js/app.js
@@ -26,8 +26,6 @@
                     })
                     .catch(function (error) { //als de url ophalen mislukt, maar werkt niet echt goed
                         // TODO: Handle your error!
-                        console.log(error)
-                        render.error()
                     })
             }
         },
@@ -52,8 +50,6 @@
                     })
                     .catch(function (error) { //als de url ophalen mislukt, maar werkt niet echt goed
                         // TODO: Handle your error!
-                        console.log(error)
-                        render.error()
                     })
             }
         }
